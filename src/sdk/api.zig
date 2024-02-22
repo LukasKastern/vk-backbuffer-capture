d5fef5ba20314a1b23d46d6ce38f8698734ac58a
Lukas Kastern <lukaskastern@web.de>
2024-02-22 11:12:48 +0100

    Debugging

--- src/sdk/api.zig
+++ src/sdk/api.zig
@@ -164,6 +164,7 @@ pub fn capture_try_get_next_frame(state: api.VKBackbufferCaptureState, wait_time
     }
 
     _ = c.pthread_mutex_lock(&backbuffer_capture_state.shared_data.texture_locks[@intCast(backbuffer_capture_state.shared_data.latest_texture)]);
+    std.log.info("Locked Texture: {}", .{backbuffer_capture_state.shared_data.latest_texture});
 
     out_frame.format = backbuffer_capture_state.shared_data.format;
     out_frame.width = backbuffer_capture_state.shared_data.width;
@@ -188,6 +189,8 @@ pub fn capture_return_frame(state: api.VKBackbufferCaptureState, frame: *const a
 
         return;
     };
+
+    std.log.info("Unlocked Texture: {}", .{frame_idx});
 
     if (c.pthread_mutex_unlock(&backbuffer_capture_state.shared_data.texture_locks[frame_idx]) == -1) {
         std.log.err("Failed to unlock mutex", .{});
